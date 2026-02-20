a13d2e6f11180ecba3ffa55c075b920221882a39
K.Felix <160111290+Xaver140@users.noreply.github.com>
2026-02-20 13:44:35 +0100

    Add files via upload

---
+++ uj-mappa2/RealEstate-master/RealEstate/Ad.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RealEstate
+{
+    public class Ad
+    {
+        public int Area { get; set; }
+        public Category Category { get; set; }
+        public DateTime CreateAt { get; set; }
+        public int Floors { get; set; }
+        public bool FreeOfCharge { get; set; }
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string LatLong { get; set; }
+        public int Rooms { get; set; }
+        public Seller Seller { get; set; }
+
+        public Ad(int id,int rooms,string latLong,int floors,int area,bool freeOfCharge,string imageUrl,DateTime createAt,Seller seller,Category category)
+        {
+            Id = id;
+            Rooms = rooms;
+            LatLong = latLong;
+            Floors = floors;
+            Area = area;
+            FreeOfCharge = freeOfCharge;
+            ImageUrl = imageUrl;
+            CreateAt = createAt;
+            Seller = seller;
+            Category = category;
+        }
+        //3. Készítsen statikus metódust az Ad osztályhoz az adatforráshoz történő kapcsolódásra, és az adatforrásban levő adatok betöltésére! A metódus neve utaljon az adatforrásból betöltésre(pl.: LoadFromCsv). A metódus visszatérési értéke az Ad osztályból képzett lista legyen! A metódus paramétere a betöltendő fájl neve legyen!
+        public static List<Ad> LoadFromCsv(string filePath)
+        {
+            var ads = new List<Ad>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines.Skip(1))
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                var fields = line.Split(';');
+                if (fields.Length < 14) continue;
+
+                var ad = new Ad(
+                    int.Parse(fields[0]), // id
+                    int.Parse(fields[1]), // rooms
+                    fields[2],            // latlong
+                    int.Parse(fields[3]), // floors
+                    int.Parse(fields[4]), // area
+                    fields[6] == "1",     // freeOfCharge 
+                    fields[7],            // imageUrl
+                    DateTime.Parse(fields[8]), // createAt
+                    new Seller(int.Parse(fields[9]), fields[10], fields[11]),
+                    new Category(int.Parse(fields[12]), fields[13])
+                );
+                ads.Add(ad);
+            }
+            return ads;
+        }
+    }
+}
---
+++ uj-mappa2/RealEstate-master/RealEstate/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+
+namespace RealEstate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath = @"C:\Users\karxav707\Documents\vizsga0213\RealEstate\RealEstate\realestates.csv";
+            List<Ad> ads = Ad.LoadFromCsv(FilePath);
+
+            //Határozza meg és írja ki a minta szerint az eladásra kínált földszinti ingatlanok átlagos alapterületét! Az eredményt a minta alapján két tizedesjegy pontossággal írja ki!
+                        Dictionary<string, List<Ad>> adsByCategory = ads.GroupBy(ad => ad.Category.Name)
+                                        .ToDictionary(group => group.Key, group => group.ToList());
+            var foldszint = ads.Where(ad => ad.Floors == 0).ToList();
+            if(foldszint.Count > 0)
+            {
+                double foldatlag = foldszint.Average(ad => ad.Area);
+                Console.WriteLine($"Az eladásra kínált fszinti ingatlanok átlag területe {foldatlag:F2} m2");
+            }
+            else
+            {
+                Console.WriteLine("Nincs ingatlan.");
+            }
+            FindClosestAd(ads);
+        }
+        //7. Készítsen DistanceTo néven valós értékkel visszatérő metódust, amelynek segítségével adott ingatlan esetében meg lehet állapítani egy GPS koordinátától való távolságot Pitagorasz-tétel segítségével!
+        public static double DistanceTo(Ad ad, string latlong)
+        {
+            var adCoords = ad.LatLong
+                .Split(',')
+                .Select(s => double.Parse(s.Trim(), CultureInfo.InvariantCulture))
+                .ToArray();
+
+            var targetCoords = latlong
+                .Split(',')
+                .Select(s => double.Parse(s.Trim(), CultureInfo.InvariantCulture))
+                .ToArray();
+
+            double dx = adCoords[0] - targetCoords[0];
+            double dy = adCoords[1] - targetCoords[1];
+
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+        //8. A Mesevár óvoda Budán a 47.4164220114023, 19.066342425796986 GPS koordinátán helyezkedik el. Keresse ki és írja ki a minta alapján annak a tehermentes ingatlannak az adatait, melyik a legközelebb van légvonalban a Mesevár óvodához!
+        public static void FindClosestAd(List<Ad> ads)
+        {
+            string oviCoords = "47.4164220114023,19.066342425796986";
+
+            var closestAd = ads
+                .Where(ad => ad.FreeOfCharge)
+                .OrderBy(ad => DistanceTo(ad, oviCoords))
+                .FirstOrDefault();
+
+            if (closestAd != null)
+            {
+                Console.WriteLine($"A legközelebbi tehermentes ingatlan adatai: \n Eladó neve:{closestAd.Seller.Name} \n Eladó telefonszáma: {closestAd.Seller.Phone} \n Szobák száma: {closestAd.Rooms} \n Alapterület: {closestAd.Area}");
+            }
+            else
+            {
+                Console.WriteLine("Nincs tehermentes ingatlan.");
+            }
+        }
+    }
+}
